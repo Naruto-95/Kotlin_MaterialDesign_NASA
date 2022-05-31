89f07652e47a2a51b1c5e18990ab2b0568673300
iliy <gromov218974@mail.ru>
2022-05-31 14:16:13 +0400

    Урок 2. Реализовал 3 темы разных цветов,ripple цвет поменял,поменял шрифт и переписал все на английский, так как с русским языком шрифт не работает. Но пока что не смог поменять шрифт в TextInputEditText

--- app/src/main/java/com/example/kotlin_materialdesign_nasa/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/kotlin_materialdesign_nasa/view/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.kotlin_materialdesign_nasa.view.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.kotlin_materialdesign_nasa.databinding.FragmentSettingsBinding
+import com.google.android.material.tabs.TabLayout
 
 
 class SettingsFragment : Fragment() {
@@ -24,6 +27,21 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Log.d("111", "${tab?.position}")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+              //  TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+              //  TODO("Not yet implemented")
+            }
+
+
+        })
 
     }
 
